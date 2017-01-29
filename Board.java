d0c58341c441dffe739ec7938b1c74eb761fe502
collineps <collineps@gmail.com>
2017-01-28 20:55:49 -0500

    Add files via upload
    PDF is original paper describing the split domain problem on the 8-puzzle. If I understand the argument, the determinant of any configuration can be positive or negative. Any positive configuration can be reached from any other positive configuration, but none can reach any configuration, and vice versa for the negative configurations. I need to double-check this though, either through further research or testing.
    
    Board class is edited to generate a random board state in the constructor. Constructor also instantiates goal state with '0'/blank being in the lower right hand corner. This goal state has a positive determinant (27). Includes method that returns determinant of current board state. Includes methods to print string representations of current board state and goal state.
    Main class has code used to test the Board class.

--- Board.java
+++ Board.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * class governing state and behavior of 8-puzzle board
  * 
@@ -6,50 +8,122 @@
  * @author Collin Epstein
  * @author Ashley Alexander-Lee
  * 
- * Time spent: 1 hour
+ * Time spent: 2 hours
  */
 public class Board {
 
 	// instance variables
 	private int[][] tiles;
 	private int[] zeroPos;
+	private int[][] goal;
 	
 	// constructor
 	public Board(int size){
 		tiles = new int[size][size];
+		goal = new int[size][size];
 		zeroPos = new int[2];
-		// create 'model' puzzle for testing
-		int cur = 0;
-		for(int i = 0; i < size; i++){ // vertical
-			for(int j = 0; j < size; j++){ // horizontal
-				tiles[i][j] = cur;
-				if (cur == 0){
-					zeroPos[0] = i;
-					zeroPos[1] = j;
+		// create random puzzle with values 0-9
+		// 0 tile represents blank/empty tile
+		int curTest;
+		int curGoal = 1;
+		Set<Integer> closed = new HashSet<Integer>();
+		Random rand = new Random();
+		boolean flag;
+		for(int i = 0; i < size; i++){ // vertical (columns)
+			for(int j = 0; j < size; j++){ // horizontal (rows)
+				flag = true;
+				while(flag){
+					curTest = rand.nextInt(size * size);
+					if(!closed.contains(curTest)){ // only add number if it's not already on board
+						tiles[i][j] = curTest;
+						if (curTest == 0){ // save empty tile location
+							zeroPos[0] = i;
+							zeroPos[1] = j;
+						}
+						closed.add(curTest); // track numbers on board
+						flag = false; // trip flag once number is added
+					}
 				}
-				cur++;
+				
+				goal[i][j] = curGoal;
+				curGoal++;
+				if(curGoal > (size * size - 1)){
+					curGoal = 0;
+				}
 			}
 		}
 	}
 	
 	/**
+	 * treats current board state as a matrix and calculates its determinant
 	 * 
+	 * implementation from:
+	 * http://stackoverflow.com/questions/29775646/finding-the-cofactor-and-determinant-of-a-3x3-matrix
+	 * user Mathias Kogler
+	 * 
+	 * @return determinant of current board state as in
+	 */
+	public int getDet(){
+		if (tiles.length == 2)
+	        return tiles[0][0] * tiles[1][1] - tiles[0][1] * tiles[1][0];
+
+	    int determinant1 = 0, determinant2 = 0;
+	    for (int i = 0; i < tiles[0].length; i++) {
+	        int temp = 1, temp2 = 1;
+	        for (int j = 0; j < tiles.length; j++) {
+	            temp *= tiles[(i + j) % tiles.length][j];
+	            temp2 *= tiles[(i + j) % tiles.length][tiles[0].length - 1 - j];
+	        }
+
+	        determinant1 += temp;
+	        determinant2 += temp2;
+	    }
+
+	    return determinant1 - determinant2;
+	}
+	
+	/**
+	 * returns representation of the current board state as a string
+	 * for testing
+	 * 
+	 * @return board represented as string
 	 */
 	public void str(){
+		System.out.println(generalString(tiles));
+	}
+	
+	/**
+	 * returns representation of goal state as a string
+	 * for testing
+	 * 
+	 * @return goal state represented as string
+	 */
+	public void goalState(){
+		System.out.println(generalString(goal));
+	}
+	
+	/**
+	 * general helper method to return a given n x n matrix representing
+	 * and n-puzzle board as a string
+	 * @param board - 2d array representing n-puzzle board
+	 * @return cur - string representation of board
+	 */
+	private String generalString(int[][] board){
 		String cur = "";
-		for(int i = 0; i < tiles[0].length; i++){ // vertical
-			for(int j = 0; j < tiles.length; j++){ // horizontal
-				cur += " " + tiles[i][j] + " |";
+		for(int i = 0; i < board[0].length; i++){ // vertical
+			for(int j = 0; j < board.length; j++){ // horizontal
+				cur += "| " + board[i][j] + " |";
 			}
 			cur += '\n';  
 		}
-		System.out.println(cur);
+		return cur;
 	}
 	
 	/**
 	 * j = horizontal direction
 	 * i = vertical direction
 	 * @param direction
+	 * @return void
 	 */
 	public void moveTile(char direction){
 		switch(direction){
--- main.java
+++ main.java
@@ -1,32 +1,55 @@
-
+/**
+ * main class to test 8-puzzle
+ * 
+ * @author Collin Epstein
+ * @author Ashley Alexander-Lee
+ *
+ */
 public class main {
 	public static void main(String args[]){
+//		int i = 0;
+//		int pos = 0;
+//		int neg = 0;
+//		int det;
 		Board board = new Board(3);
 		board.str();
-		System.out.println();
+		board.goalState();
+//		while(i < 100){
+//			board = new Board(3);
+//			//board.str();
+//			det = board.getDet();
+//			if(det >= 0){
+//				pos++;
+//			} else { 
+//				neg++;
+//			}
+//			i++;
+//		}
+//		System.out.println("Positive Determinants: " + pos + '\n' + 
+//				"Negative Determinants: " + neg);
 		
-		board.moveTile('n');
-		board.moveTile('s');
-		System.out.println();
-		board.str();
-		
-		board.moveTile('e');
-		System.out.println();
-		board.str();
-		
-		board.moveTile('w');
-		System.out.println();
-		board.str();
-		board.moveTile('w');
-		
-		board.moveTile('x');
-		
-		board.moveTile('e');
-		board.moveTile('e');
-		board.moveTile('e');
-		board.str();
-		board.moveTile('s');
-		board.moveTile('s');
-		board.str();
+//		board.moveTile('n');
+//		board.moveTile('s');
+//		System.out.println();
+//		board.str();
+//		
+//		board.moveTile('e');
+//		System.out.println();
+//		board.str();
+//		
+//		board.moveTile('w');
+//		System.out.println();
+//		board.str();
+//		board.moveTile('w');
+//		
+//		board.moveTile('x');
+//		
+//		board.moveTile('e');
+//		board.moveTile('e');
+//		board.moveTile('e');
+//		board.str();
+//		board.moveTile('s');
+//		board.moveTile('s');
+//		board.str();
 	}
 }
