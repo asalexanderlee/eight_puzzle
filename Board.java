d4e67259927fa20a91322e296ad51050720579de
asalexanderlee <asalexanderlee@davidson.edu>
2017-01-31 23:49:06 -0500

    Add files via upload

--- Board.java
+++ Board.java
@@ -2,6 +2,7 @@ package eightpuzzle;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -15,15 +16,19 @@ import java.util.Set;
  * 
  * Time spent: 5 hours
  */
-public class Board {
+public class Board implements Comparable<Board>{
 
 	// instance variables
 	private int[] tiles;
 	private int zeroPos;
-	
+	private int heur;
+	private int moves;
+
 	// constructor
-	public Board(){
+	public Board(int h){
 		tiles = new int[9]; // could create n-puzzle in theory
+		heur = h;
+		moves = 0;
 		//goal = new int[9]; // but for this assignment we are only concerned with 8-puzzle
 		// create random puzzle with values 0-9
 		// 0 tile represents blank/empty tile
@@ -48,11 +53,13 @@ public class Board {
 		}
 	}
 
-	public Board(int[] state, int zero){
+	public Board(int[] state, int zero, int h, int m){
 		tiles = state;
 		zeroPos = zero;
+		heur = h;
+		moves = m;
 	}
-	
+
 	/**
 	 * 
 	 */
@@ -63,11 +70,11 @@ public class Board {
 				cur += '\n';
 			}
 			cur += " " + tiles[i] + " |";
-				
+
 		} 
 		System.out.println(cur);
 	}
-	
+
 	/**
 	 * j = horizontal direction
 	 * i = vertical direction
@@ -119,75 +126,117 @@ public class Board {
 			break;
 		}
 	}*/
-  
-  	public ArrayList<Board> getMoves(){
-      
-      ArrayList<Board> posMoves = new ArrayList<Board>();
-      
-      boolean[] checks = {(zeroPos % 3) < 2, (zeroPos % 3) >= 0, (zeroPos) >= 3, (zeroPos) < 6};
-      int[] movts = {zeroPos+1, zeroPos-1, zeroPos-3, zeroPos+3};
-      int i = 0;
-      
-      while (i < 4){
-	      if (checks[i]){
-	    	  int[] copy = tiles.clone();
-	    	  int temp = copy[movts[i]];
-	    	  copy[movts[i]] = 0;
-	    	  copy[zeroPos] = temp;
-	    	  posMoves.add(new Board(copy, movts[i]));
-	      }
-	      i++;
-      }
-      
-      return posMoves;
-  	}
-  	
-  	
-  	public int heuristic1(){
-  		
-  		int numMisplaced = 0;
-  		
-  		for (int i = 0; i < tiles.length; i++){
-  			if (tiles[i] != i){
-  				numMisplaced++;
-  			}
-  		}
-  		
-  		return numMisplaced;
-  	}
-  	
-  	/*private int findDisplacemt(int num){
-  		
-  		int[] goalPlace = new int[2];
-  		int[] tilesPlace = new int[2];
-  		boolean goalPlaceFound = false;
-  		boolean tilesPlaceFound = false;
-  		
-  		for (int i = 0; i < tiles.length; i++){
-  			for (int j = 0; j < tiles[0].length; j++){
-  				if (tiles[i][j] == num){
-  					tilesPlace[0] = i;
-  					tilesPlace[1] = j;
-  				}
-  				if (goal[i][j] == num){
-  					goalPlace[0] = i;
-  					goalPlace[1] = j;
-  				}
-  				if (goalPlaceFound && tilesPlaceFound){
-  					break;
-  				}
-  			}
-  		}
-  		return Math.abs(goalPlace[0]-tilesPlace[0]) + Math.abs(goalPlace[1]-tilesPlace[1]);
-  	}*/
-  	public int heuristic2(){
-  		
-  		int totalMovt = 0;
-  		
-  		for (int i = 0; i < tiles.length; i++){
-  			totalMovt += Math.abs(i - tiles[i])%3 + Math.abs(i - tiles[i])%3;
-  		}
-  	
-  		return totalMovt;
-  	}
+
+	public List<Board> getMoves(){
+
+		List<Board> posMoves = new ArrayList<Board>();
+
+		boolean[] checks = {(zeroPos % 3) < 2, (zeroPos % 3) > 0, (zeroPos) > 3, (zeroPos) < 6};
+		int[] movts = {zeroPos+1, zeroPos-1, zeroPos-3, zeroPos+3};
+		int i = 0;
+
+		while (i < 4){
+			if (checks[i]){
+				int[] copy = tiles.clone();
+				int temp = copy[movts[i]];
+				copy[movts[i]] = 0;
+				copy[zeroPos] = temp;
+				posMoves.add(new Board(copy, movts[i], this.heur, this.moves + 1));
+			}
+			i++;
+		}
+		return posMoves;
+	}
+
+
+	private int heuristic1(){
+
+		int numMisplaced = 0;
+
+		for (int i = 0; i < tiles.length; i++){
+			if (tiles[i] != i && tiles[i] != 0){
+				numMisplaced++;
+			}
+		}
+
+		return numMisplaced;
+	}
+
+	private int heuristic2(){
+
+		int totalMovt = 0;
+
+		for (int i = 0; i < tiles.length; i++){
+			if (tiles[i] != 0){
+				totalMovt += Math.abs(tiles[i]/3 - i/3) + Math.abs(tiles[i]%3 - i%3);
+			}
+		}
+
+		return totalMovt;
+	}
+
+	public int compareTo(Board o) {
+		return (this.heuristic() + this.getNumberMoved()) - (o.heuristic() + o.getNumberMoved());
+	}
+	
+	public int getUsedHeur(){
+		return heur;
+	}
+
+	public boolean isSolvable(){
+		// count number of inversions on board
+		int inversions = 0;
+		for(int i = 0; i < tiles.length; i++){
+			for(int j = i + 1; j < tiles.length; j++){
+				if(tiles[j] > tiles[i] && tiles[j] != 0 && tiles[i]!=0){
+					inversions++;
+				}
+			}
+		}
+		//System.out.println(inversions);
+		// check parity of inversions
+		if(inversions % 2 == 0){ // even = solvable
+			//System.out.println("Solvable puzzle.");
+			return true;
+		} else { // odd = unsolvable
+			//System.out.println("Unsolvable puzzle.");
+			return false;
+		}
+	}
+	
+	public int heuristic(){
+		if(heur == 1){
+			return this.heuristic1();
+		}
+		else if (heur == 2){
+			return this.heuristic2();
+		} else {
+			return 0; //EXCEPTION NEEDED?
+		}
+	}
+	
+	public int getNumberMoved(){
+		return moves;
+	}
+	
+	public boolean equals(Object other){
+		Board o = (Board) other;
+		for(int i = 0; i < this.tiles.length; i++){
+			if(o.tiles[i] != this.tiles[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public int hashCode(){
+		int[] prime = {1,2,5,7,13,17,19,23,29};
+		int code = 0;
+		for(int i = 0; i < tiles.length; i++){
+			code += tiles[i] * prime[i];
+		}
+		return code;
+	}
+	
+	
 }
