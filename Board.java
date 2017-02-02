7b8c97c98408d2ae9dd2d4f68cd557e9d2077b1e
collineps <collineps@gmail.com>
2017-02-01 20:00:33 -0500

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,63 +1,103 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * class governing state and behavior of 8-puzzle board
+ * blank tile represented as number 0
  * 
  * AI HW1
  * 
  * @author Collin Epstein
  * @author Ashley Alexander-Lee
- * 
- * Time spent: 2 hours
  */
-public class Board {
+public class Board implements Comparable<Board>{
 
 	// instance variables
-	private int[][] tiles;
-	private int[] zeroPos;
-	private int[][] goal;
-	
-	// constructor
-	public Board(){
-		tiles = new int[3][3]; // could create n-puzzle in theory
-		goal = new int[3][3]; // but for this assignment we are only concerned with 8-puzzle
-		zeroPos = new int[2];
-		// create random puzzle with values 0-9
-		// 0 tile represents blank/empty tile
+	private int[] tiles; // board state
+	private int zeroPos; // index of blank
+	private int heur; // heuristic used
+	private int moves; // moves to get to current state
+
+	/**
+	 * random constructor
+	 * 
+	 * constructs new randomized board that will used specified heuristic function
+	 * 
+	 * @param h - int specifying heuristic function to use
+	 */
+	public Board(int h){
+		tiles = new int[9]; // represent board state as 1D array
+		heur = h;
+		moves = 0;
+
+		// randomly generate initial state of board
 		int curInit;
-		int curGoal = 1;
 		Set<Integer> closed = new HashSet<Integer>();
 		Random rand = new Random();
 		boolean flag;
-		for(int i = 0; i < tiles[0].length; i++){ // vertical (columns)
-			for(int j = 0; j < tiles.length; j++){ // horizontal (rows)
-				flag = true;
-				while(flag){
-					curInit = rand.nextInt(tiles[0].length * tiles.length);
-					if(!closed.contains(curInit)){ // only add number if it's not already on board
-						tiles[i][j] = curInit;
-						if (curInit == 0){ // save empty tile location
-							zeroPos[0] = i;
-							zeroPos[1] = j;
-						}
-						closed.add(curInit); // track numbers on board
-						flag = false; // trip flag once number is added
+		for(int i = 0; i < tiles.length; i++){
+			flag = true;
+			while(flag){
+				curInit = rand.nextInt(tiles.length); // generate random number in range
+				if(!closed.contains(curInit)){ // only add number if it's not already on board
+					tiles[i] = curInit;
+					if (curInit == 0){ // save blank tile location
+						zeroPos = i;
 					}
-				}
-				
-				goal[i][j] = curGoal;
-				curGoal++;
-				if(curGoal > (8)){
-					curGoal = 0;
+					closed.add(curInit); // track numbers on board
+					flag = false; // trip flag once number is added
 				}
 			}
+
 		}
+	}
+
+	/**
+	 * specific constructor
+	 * 
+	 * constructs new board object with specific state and heuristic function
+	 * counts number of moves taken to get to current state
+	 * 
+	 * @param state - array of int describing board state
+	 * @param zero - index of blank tiles (0)
+	 * @param h - int specifying heuristic
+	 * @param m - int number of moves taken to reach current state
+	 */
+	public Board(int[] state, int zero, int h, int m){
+		tiles = state;
+		zeroPos = zero;
+		heur = h;
+		moves = m;
 	}
 	
 	/**
-	 * determines whether current board is solvable
-	 * counts inversions in order of tiles
-	 * even = solvable, odd = unsolvable
+	 * getter for the number of the heuristic used for this board
+	 * 
+	 * @return int - heuristic ID number
+	 */
+	public int getUsedHeur(){
+		return heur;
+	}
+
+	/**
+	 * getter for number of moves taken to reach current state
+	 * 
+	 * @return int - number of moves
+	 */
+	public int getNumberMoved(){
+		return moves;
+	}
+
+	/**
+	 * determines whether the current board is in a solvable configuration
+	 * 
+	 * counts inversions (numbers in array that are 'out of order' and by how much)
+	 * parity determines solvability:
+	 * even = solvable
+	 * odd = unsolvable
 	 * 
 	 * based on solutions described by Mark Ryan:
 	 * http://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
@@ -65,180 +105,220 @@ public class Board {
 	 * and code by Tushar Vaghela
 	 * http://math.stackexchange.com/questions/293527/how-to-check-if-a-8-puzzle-is-solvable
 	 * 
-	 * @return bool describing whether puzzle can be solved
+	 * @return boolean - whether board can be solved or not
 	 */
 	public boolean isSolvable(){
-		
-		// convert board to 1D array
-		int[] line = new int[tiles[0].length * tiles.length - 1];
-		int index = 0;
-		String list = "";
-		for(int i = 0; i < tiles[0].length; i++){ // col
-			for(int j = 0; j < tiles.length; j++){ // row
-				if(tiles[i][j] != 0){
-					line[index] = tiles[i][j];
-					index++;
-					list += tiles[i][j];
-				}
-			}
-		}
-		//System.out.println(list); // check array
-		
 		// count number of inversions on board
 		int inversions = 0;
-		for(int i = 0; i < line.length; i++){
-			for(int j = i + 1; j < line.length; j++){
-				if(line[j] > line[i]){
+		for(int i = 0; i < tiles.length; i++){
+			for(int j = i + 1; j < tiles.length; j++){
+				// don't count blank tile
+				if(tiles[j] > tiles[i] && tiles[j] != 0 && tiles[i]!=0){
 					inversions++;
 				}
 			}
 		}
-		//System.out.println(inversions);
 		// check parity of inversions
 		if(inversions % 2 == 0){ // even = solvable
-			//System.out.println("Solvable puzzle.");
 			return true;
 		} else { // odd = unsolvable
-			//System.out.println("Unsolvable puzzle.");
 			return false;
 		}
-		
 	}
 	
 	/**
-	 * getter for current board state as 3x3 array
+	 * generates and returns all possible moves for the given board state
 	 * 
-	 * @return tiles - current board state as array
+	 * @return list of boards - child nodes of current state
 	 */
-	public int[][] getState(){
-		return tiles;
+	public List<Board> getMoves(){
+
+		List<Board> posMoves = new ArrayList<Board>();
+
+		boolean[] checks = {(zeroPos % 3) < 2, (zeroPos % 3) > 0, (zeroPos) > 3, (zeroPos) < 6};
+		int[] movts = {zeroPos+1, zeroPos-1, zeroPos-3, zeroPos+3}; // left, right, up down
+		int i = 0;
+
+		while (i < 4){ // all four direction possibilities
+			if (checks[i]){ // if movement is allowed
+				int[] copy = tiles.clone();
+				int temp = copy[movts[i]];
+				copy[movts[i]] = 0;
+				copy[zeroPos] = temp;
+				posMoves.add(new Board(copy, movts[i], this.heur, this.moves + 1)); // add child
+			}
+			i++;
+		}
+		return posMoves;
 	}
 	
 	/**
-	 * getter for goal state as 3x3 array
+	 * returns heuristic value for the given heuristic
 	 * 
-	 * @return goal - goal state as array
+	 * evaluates heuristic function in helper method
+	 * depends on heuristic specified in board
+	 * 
+	 * @return int - value of heuristic function for current state
 	 */
-	public int[][] getGoal(){
-		return goal;
+	public int heuristic(){
+		if(heur == 1){
+			return this.heuristic1();
+		}
+		else if (heur == 2){
+			return this.heuristic2();
+		} 
+		else if (heur == 3){
+			return this.heuristic3();
+		}else {
+			return 0; // effectively don't evaluate problem if invalid heuristic is specified
+		}
 	}
-	
+
 	/**
-	 * treats current board state as a matrix and calculates its determinant
+	 * misplaced tiles heuristic function
 	 * 
-	 * implementation from:
-	 * http://stackoverflow.com/questions/29775646/finding-the-cofactor-and-determinant-of-a-3x3-matrix
-	 * user Mathias Kogler
+	 * counts number of tiles that are not in the 'correct' location
 	 * 
-	 * @return determinant of current board state as in
+	 * @return int - value of heuristic function 1 for current state
 	 */
-	public int getDet(){
-		if (tiles.length == 2)
-	        return tiles[0][0] * tiles[1][1] - tiles[0][1] * tiles[1][0];
+	private int heuristic1(){
 
-	    int determinant1 = 0, determinant2 = 0;
-	    for (int i = 0; i < tiles[0].length; i++) {
-	        int temp = 1, temp2 = 1;
-	        for (int j = 0; j < tiles.length; j++) {
-	            temp *= tiles[(i + j) % tiles.length][j];
-	            temp2 *= tiles[(i + j) % tiles.length][tiles[0].length - 1 - j];
-	        }
+		int numMisplaced = 0;
 
-	        determinant1 += temp;
-	        determinant2 += temp2;
-	    }
+		// tile is misplaced if array value doesn't match array index
+		for (int i = 0; i < tiles.length; i++){
+			if (tiles[i] != i && tiles[i] != 0){ // don't count blank tile
+				numMisplaced++;
+			}
+		}
 
-	    return determinant1 - determinant2;
+		return numMisplaced;
+	}
+
+	/**
+	 * manhattan distance heuristic function
+	 * 
+	 * counts vertical and horizontal displacement of all tiles from their 'correct' locations
+	 * 
+	 * row = x / 3
+	 * column = x % 3
+	 * 
+	 * @return int - value of heuristic function 2 for current state
+	 */
+	private int heuristic2(){
+
+		int totalMovt = 0;
+
+		for (int i = 0; i < tiles.length; i++){
+			if (tiles[i] != 0){ // don't count blank tile
+				totalMovt += Math.abs(tiles[i]/3 - i/3) + Math.abs(tiles[i]%3 - i%3);
+			}
+		}
+
+		return totalMovt;
 	}
 	
 	/**
-	 * returns representation of the current board state as a string
-	 * for testing
+	 * out of column/row heuristic function
 	 * 
-	 * @return board represented as string
+	 * counts number of tiles that are displace from 'correct' row and 'correct' column
+	 * 
+	 * row = x / 3
+	 * column = x % 3
+	 * 
+	 * from https://heuristicswiki.wikispaces.com/Tiles+out+of+row+and+column
+	 * 
+	 * @return int - value of heuristic function 3 for current state
+	 */
+	private int heuristic3(){
+		int total = 0;
+		for (int i = 0; i< tiles.length; i++){
+			if (tiles[i] != 0){ // don't count blank tile
+				if (tiles[i]/3 != i/3){
+					total++;
+				}
+				if (tiles[i]%3 != i%3){
+					total++;
+				}
+			}
+		}
+		return total;
+	}
+
+	/**
+	 * overwritten method to implement Comparable interface
+	 * 
+	 * determines whether a current board is 'more than', 'less than' or 'equal to' a given board 
+	 * compares boards based on function:
+	 * f(n) = g(n) + h(n)
+	 * where g(n) is the number of moves taken to reach the current state
+	 * and h(n) is the value of the heuristic function at the current state
+	 * 
+	 * https://docs.oracle.com/javase/8/docs/api/java/lang/Comparable.html#compareTo-T-
+	 * 
+	 * @param o - other board object to compare to
+	 * @return int - negative if this is smaller than other, 0 if this equals other, 
+	 * 				positive if this is larger than other
+	 */
+	public int compareTo(Board o) {
+		return (this.heuristic() + this.getNumberMoved()) - (o.heuristic() + o.getNumberMoved());
+	}
+	
+	/**
+	 * overwritten method to enable use with sets
+	 * 
+	 * determines whether two boards are 'equal'
+	 * compares state of board (locations of tiles)
+	 * 
+	 * @param other - other board object to compare to
+	 * @return boolean - whether two boards are equal
+	 */
+	public boolean equals(Object other){
+		Board o = (Board) other; // cast to board to overwrite method
+		for(int i = 0; i < this.tiles.length; i++){
+			if(o.tiles[i] != this.tiles[i]){ // compare tile states
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * overwritten method to enable hashing
+	 * 
+	 * determines hash code based on state of board
+	 * (may not be necessary?)
+	 * 
+	 * @return int - board hash code
+	 */
+	public int hashCode(){
+		int[] prime = {1,2,5,7,13,17,19,23,29};
+		int code = 0;
+		for(int i = 0; i < tiles.length; i++){
+			code += tiles[i] * prime[i];
+		}
+		return code;
+	}
+	
+	/**
+	 * prints string representation of 3x3 board
+	 * 
+	 * eg:
+	 * 0 | 1 | 2 |
+	 * 3 | 4 | 5 |
+	 * 6 | 7 | 8 |
+	 * 
+	 * used primarily for testing
 	 */
 	public void str(){
-		System.out.println(generalString(tiles));
-	}
-	
-	/**
-	 * returns representation of goal state as a string
-	 * for testing
-	 * 
-	 * @return goal state represented as string
-	 */
-	public void goalState(){
-		System.out.println(generalString(goal));
-	}
-	
-	/**
-	 * general helper method to return a given n x n matrix representing
-	 * and n-puzzle board as a string
-	 * @param board - 2d array representing n-puzzle board
-	 * @return cur - string representation of board
-	 */
-	private String generalString(int[][] board){
 		String cur = "";
-		for(int i = 0; i < board[0].length; i++){ // vertical
-			for(int j = 0; j < board.length; j++){ // horizontal
-				cur += "| " + board[i][j] + " |";
+		for(int i = 0; i < tiles.length; i++){ 
+			if(i % 3 == 0){
+				cur += '\n'; // new line every 3 tiles
 			}
-			cur += '\n';  
-		}
-		return cur;
-	}
-	
-	/**
-	 * j = horizontal direction
-	 * i = vertical direction
-	 * @param direction
-	 * @return void
-	 */
-	public void moveTile(char direction){
-		switch(direction){
-		case 'a': // move blank left
-			if(zeroPos[1] - 1 >= 0){
-				tiles[zeroPos[0]][zeroPos[1]] = tiles [zeroPos[0]] [zeroPos[1] - 1];
-				tiles [zeroPos[0]] [zeroPos[1] - 1] = 0;
-				zeroPos[1] -= 1;
-			} else {
-				System.out.println("Move is not valid.");
-			}
-			break;
-		case 'd': // move blank right
-			if(zeroPos[1] + 1 <= tiles.length - 1){
-				tiles[zeroPos[0]][zeroPos[1]] = tiles [zeroPos[0]] [zeroPos[1] + 1];
-				//System.out.println(tiles[zeroPos[0]][zeroPos[1]]);
-				tiles [zeroPos[0]] [zeroPos[1] + 1] = 0;
-				zeroPos[1] += 1;
-				//System.out.println("Zero Position: (" + zeroPos[0] + ", " + zeroPos[1] + ")");
-			} else {
-				System.out.println("Move is not valid.");
-			}
-			break;
-		case 's': // move blank down
-			if(zeroPos[0] + 1 <= tiles.length - 1){
-				tiles[zeroPos[0]][zeroPos[1]] = tiles [zeroPos[0] + 1] [zeroPos[1]];
-				tiles [zeroPos[0] + 1] [zeroPos[1]] = 0;
-				zeroPos[0] += 1;
-			} else {
-				System.out.println("Move is not valid.");
-			}
-			break;
-		case 'w': // move blank up
-			if(zeroPos[0] - 1 >= 0){
-				tiles[zeroPos[0]][zeroPos[1]] = tiles [zeroPos[0] - 1] [zeroPos[1]];
-				tiles [zeroPos[0] - 1] [zeroPos[1]] = 0;
-				zeroPos[0] -= 1;
-			} else {
-				System.out.println("Move is not valid.");
-			}
-			break;
-		default:
-			System.out.println("Must move up (w), down (s),"
-					+ " left (a), or right (d).");
-			break;
-		}
+			cur += " " + tiles[i] + " |";
+
+		} 
+		System.out.println(cur);
 	}
 }
-
--- main.java
+++ main.java
@@ -1,82 +1,141 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
 /**
- * main class to test 8-puzzle
+ * main class to run A* algorithm on 8-puzzle implementation
  * 
  * @author Collin Epstein
  * @author Ashley Alexander-Lee
  *
  */
 public class main {
+
+	/**
+	 * runs depth-limited A* algorithm on a given board
+	 * 
+	 * uses any of 3 heuristics:
+	 * -misplaced tiles
+	 * -manhattan distance
+	 * -tiles out of row and column
+	 * 
+	 * returns solution depth and number of nodes generated
+	 * 
+	 * use d = 100 or something large to eliminate depth limitation
+	 * 8-puzzle solution lengths limited to ~30  (research this?)
+	 * 
+	 * @param board - an 8-puzzle board object
+	 * @param d - int solution depth limit
+	 * @return int[] - 0: # of nodes generated, 1: solution depth
+	 */
+	private static int[] aStar(Board board, int d){
+
+		Queue<Board> frontier = new PriorityQueue<Board>();
+		Set<Board> visited = new HashSet<Board>();
+		int nodeCount = 0;
+
+		frontier.add(board); // initialize problem
+
+		while (!frontier.isEmpty()){ // return failure if frontier is empty
+			Board curNode = frontier.poll(); // pop node
+			if(curNode.heuristic() == 0){ // goal state at heuristic = 0
+				int[] answer = {nodeCount, curNode.getNumberMoved()};
+				return answer;
+			}
+			List<Board> possibleMoves = curNode.getMoves(); 
+			for(int i = 0; i < possibleMoves.size(); i++){
+				Board child = possibleMoves.get(i); // add all successors to queue
+				// no repeat visits and depth-limited
+				if(!visited.contains(child) && child.getNumberMoved() <= d){
+					frontier.add(child);
+					visited.add(child);
+				}
+				nodeCount++;
+			}
+		}
+
+		return new int[2]; //[0,0] indicates no solution possible
+	}
+
+	/**
+	 * run depth-limited A* multiple times and record resulting data to file
+	 * 
+	 * generates random boards, solves, records and writes data on:
+	 * -solution depth
+	 * -number of boards generated of each solution depth
+	 * -average number of nodes generated in board of each solution depth
+	 * 
+	 * writes data to .txt file
+	 * adapted from code from users Michael and EJP at:
+	 * http://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-a-file-in-java
+	 * 
+	 * @param n - number of boards to generate and run A* on
+	 * @param h - heuristic to use in A*
+	 * @param d - int solution depth limit
+	 */
+	private static void generateRandSolutions(int n, int h, int d){
+		
+		//List: 0: num Nodes, 1: num Boards generated
+		Map<Integer,List<Integer>> solutions = new HashMap<Integer,List<Integer>>(); 
+		int i = 0;
+
+		while (i < n){ // run algorithm many times
+			
+			Board board = new Board(h);
+			while(!board.isSolvable()){ // only pass solvable board to A*
+				board = new Board(h);
+			}
+			
+			int[] aStarAns = aStar(board, d); // 0: num nodes, 1: depth
+			// if solution of found depth exists in map, increase number of solutions found
+			// and calculate average number of nodes found
+			if (solutions.containsKey(aStarAns[1])){ 
+				List<Integer> value = solutions.get(aStarAns[1]);
+				if(value.get(1) < 100){ // save and average 1st 100 boards of given length
+					value.set(0, value.get(0) + aStarAns[0]);
+					value.set(1, value.get(1)+1);
+					solutions.put(aStarAns[1], value);
+				}
+			}
+			else{ // if no solution of found depth exists in map, create entry
+				List<Integer> value = new ArrayList<Integer>();
+				value.add(0, aStarAns[0]);
+				value.add(1, 1);
+				solutions.put(aStarAns[1], value);
+			}
+			i++;
+		}
+
+		Set<Integer> keys = solutions.keySet();
+		try{ // write all data to text file
+			PrintWriter writer = new PrintWriter("aStar" + h + "." + d + "." + n + ".txt", "UTF-8");
+
+			writer.println("Heuristic " + h + ", Depth limit = " + d + ", Trials = " + n);
+			writer.println("Note: dpeth = 0 and # of nodes = 0 indicates discarded solutions.");
+			for (int k: keys){
+				writer.println("d: " + k + ", Average Nodes Generated: " + 
+						solutions.get(k).get(0)/solutions.get(k).get(1) + ", Number of boards: " + solutions.get(k).get(1));
+			}
+			writer.close();
+		}catch (IOException e) {
+			// do something
+		}
+
+	}
+	
+	// main method
 	public static void main(String args[]){
-		
-		// code to actively play game in console
-		// use WASD command to move blank tiles
-		Scanner console = new Scanner(System.in);
-		Board board = new Board();
-		
-		while(!board.isSolvable()){
-			board = new Board();
-		}
-		
-		System.out.println("8-puzzle game begin!");
-		board.str();
-		
-		while(board.isSolvable() && board.getState() != board.getGoal()){
-			System.out.println("Your guess?");
-			char command = console.next().toLowerCase().charAt(0);
-			//System.out.println();
-			board.moveTile(command);
-			board.str();
-		}
-		
-		console.close();
-		// game code ends
-		
-		// code to generate series of randomly generated board
-		// and check how many boards are solvable
-//		Board board;
-//		int i = 0;
-//		int yes = 0;
-//		int no = 0;
-//		boolean solvable;
-//		while(i < 100){
-//			board = new Board();
-//			//board.str();
-//			solvable = board.isSolvable();
-//			if(solvable){
-//				yes++;
-//			} else { 
-//				no++;
-//			}
-//			i++;
-//		}
-//		System.out.println("Solvable Boards: " + yes + '\n' + 
-//				"Unsolvable Boards: " + no);
-		// end solvability test code
-		
-//		board.moveTile('n');
-//		board.moveTile('s');
-//		System.out.println();
-//		board.str();
-//		
-//		board.moveTile('e');
-//		System.out.println();
-//		board.str();
-//		
-//		board.moveTile('w');
-//		System.out.println();
-//		board.str();
-//		board.moveTile('w');
-//		
-//		board.moveTile('x');
-//		
-//		board.moveTile('e');
-//		board.moveTile('e');
-//		board.moveTile('e');
-//		board.str();
-//		board.moveTile('s');
-//		board.moveTile('s');
-//		board.str();
+
+		generateRandSolutions(1000000, 1, 10);
+		generateRandSolutions(1000000, 2, 10);
+		generateRandSolutions(1000000, 3, 10);
+
 	}
 }
